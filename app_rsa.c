7d8c13c5fbdbfbca5fb57b6c0241fe94ed669c02
seb-fae <71495014+seb-fae@users.noreply.github.com>
2022-01-21 11:00:33 +0100

    Add sign/verify bench

--- app_rsa.c
+++ app_rsa.c
@@ -51,21 +51,9 @@
 #define KEY_SIZE 2048
 #define EXPONENT 65537
 
-#if !defined(MBEDTLS_BIGNUM_C) || !defined(MBEDTLS_ENTROPY_C) ||   \
-    !defined(MBEDTLS_RSA_C) || !defined(MBEDTLS_GENPRIME) ||      \
-    !defined(MBEDTLS_CTR_DRBG_C)
-int app_rsa_key_gen( void )
-{
-    mbedtls_printf("MBEDTLS_BIGNUM_C and/or MBEDTLS_ENTROPY_C and/or "
-           "MBEDTLS_RSA_C and/or MBEDTLS_GENPRIME and/or "
-           "MBEDTLS_FS_IO and/or MBEDTLS_CTR_DRBG_C not defined.\n");
-    mbedtls_exit( 0 );
-}
-#else
-
-
 mbedtls_mpi N, P, Q, D, E, DP, DQ, QP;
 unsigned char encrypted_buf[512];
+char data_to_encrypt[] = "This is the message to encrypt with RSA";
 
 uint32_t time_tick;
 uint32_t time_diff_tick;
@@ -114,7 +102,7 @@ void app_rsa_key_gen( void )
     time_diff_tick = sl_sleeptimer_get_tick_count() - time_tick;
     time_diff_ms = sl_sleeptimer_tick_to_ms(time_diff_tick);
 
-    mbedtls_printf( " ok  clock cycles: %ld, time: %" PRIu32 " ms.\n\n Exporting the public  key ....", time_diff_tick, time_diff_ms );
+    mbedtls_printf( " ok  clock cycles: %ld, time: %" PRIu32 " ms\n  . Exporting the public  key ....", time_diff_tick, time_diff_ms );
 
 
     if( ( ret = mbedtls_rsa_export    ( &rsa, &N, &P, &Q, &D, &E ) ) != 0 ||
@@ -123,46 +111,6 @@ void app_rsa_key_gen( void )
         mbedtls_printf( " failed\n  ! could not export RSA parameters\n\n" );
         goto exit;
     }
-
-#ifdef FILESYSTEM
-    FILE *fpub  = NULL;
-    FILE *fpriv = NULL;
-
-    if( ( fpub = fopen( "rsa_pub.txt", "wb+" ) ) == NULL )
-    {
-        mbedtls_printf( " failed\n  ! could not open rsa_pub.txt for writing\n\n" );
-        goto exit;
-    }
-
-    if( ( ret = mbedtls_mpi_write_file( "N = ", &N, 16, fpub ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "E = ", &E, 16, fpub ) ) != 0 )
-    {
-        mbedtls_printf( " failed\n  ! mbedtls_mpi_write_file returned %d\n\n", ret );
-        goto exit;
-    }
-
-    mbedtls_printf( " ok\n  . Exporting the private key in rsa_priv.txt..." );
-
-
-    if( ( fpriv = fopen( "rsa_priv.txt", "wb+" ) ) == NULL )
-    {
-        mbedtls_printf( " failed\n  ! could not open rsa_priv.txt for writing\n" );
-        goto exit;
-    }
-
-    if( ( ret = mbedtls_mpi_write_file( "N = " , &N , 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "E = " , &E , 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "D = " , &D , 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "P = " , &P , 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "Q = " , &Q , 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "DP = ", &DP, 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "DQ = ", &DQ, 16, fpriv ) ) != 0 ||
-        ( ret = mbedtls_mpi_write_file( "QP = ", &QP, 16, fpriv ) ) != 0 )
-    {
-        mbedtls_printf( " failed\n  ! mbedtls_mpi_write_file returned %d\n\n", ret );
-        goto exit;
-    }
-#endif
 
     mbedtls_printf( " ok\n\n" );
 
@@ -173,26 +121,146 @@ exit:
     mbedtls_ctr_drbg_free( &ctr_drbg );
     mbedtls_entropy_free( &entropy );
 }
-#endif /* MBEDTLS_BIGNUM_C && MBEDTLS_ENTROPY_C && MBEDTLS_RSA_C &&
-          MBEDTLS_GENPRIME && MBEDTLS_FS_IO && MBEDTLS_CTR_DRBG_C */
 
+/********************************************* Sign/verify **********************/
 
-/*******************************************************************************
- *******************************   DEFINES   ***********************************
- ******************************************************************************/
-#if !defined(MBEDTLS_BIGNUM_C) || !defined(MBEDTLS_RSA_C) ||  \
-    !defined(MBEDTLS_ENTROPY_C) | \
-    !defined(MBEDTLS_CTR_DRBG_C)
-int app_rsa_encrypt( void )
+unsigned char din[2048] = "this is the data to sign";
+unsigned char sign[MBEDTLS_MPI_MAX_SIZE];
+
+void app_rsa_sign()
 {
-    mbedtls_printf("MBEDTLS_BIGNUM_C and/or MBEDTLS_RSA_C and/or "
-           "MBEDTLS_ENTROPY_C and/or MBEDTLS_FS_IO and/or "
-           "MBEDTLS_CTR_DRBG_C not defined.\n");
-    mbedtls_exit( 0 );
-}
-#else
+    int ret = 1;
+    mbedtls_rsa_context rsa;
+    unsigned char hash[32];
+    mbedtls_entropy_context entropy;
+    mbedtls_ctr_drbg_context ctr_drbg;
+    const char *pers = "rsa_sign";
 
-char data_to_encrypt[] = "This is the message to encrypt with RSA";
+    mbedtls_rsa_init( &rsa );
+    mbedtls_ctr_drbg_init( &ctr_drbg );
+    mbedtls_entropy_init( &entropy );
+
+    ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func,
+                                        &entropy, (const unsigned char *) pers,
+                                        strlen( pers ) );
+
+    mbedtls_printf( "\n  . Import private key" );
+
+
+    if( ( ret = mbedtls_rsa_import( &rsa, &N, &P, &Q, &D, &E ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_import returned %d\n\n",
+                        ret );
+        goto exit;
+    }
+
+    if( ( ret = mbedtls_rsa_complete( &rsa ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_complete returned %d\n\n",
+                        ret );
+        goto exit;
+    }
+
+    mbedtls_printf( "\n  . Checking the private key" );
+
+    if( ( ret = mbedtls_rsa_check_privkey( &rsa ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_check_privkey failed with -0x%0x\n", (unsigned int) -ret );
+        goto exit;
+    }
+
+    /*
+     * Compute the SHA-256 hash of the input file,
+     * then calculate the RSA signature of the hash.
+     */
+    mbedtls_printf( "\n  . Generating the RSA/SHA-256 signature" );
+
+
+    if( ( ret = mbedtls_md(
+                    mbedtls_md_info_from_type( MBEDTLS_MD_SHA256 ),
+                    din, sizeof(din), hash ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! Could not hash input \n\n" );
+        goto exit;
+    }
+
+    time_tick = sl_sleeptimer_get_tick_count();
+
+    if( ( ret = mbedtls_rsa_pkcs1_sign( &rsa, mbedtls_ctr_drbg_random,
+                                        &ctr_drbg, MBEDTLS_MD_SHA256,
+                                32, hash, sign ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_pkcs1_sign returned -0x%0x\n\n", (unsigned int) -ret );
+        goto exit;
+    }
+
+
+    time_diff_tick = sl_sleeptimer_get_tick_count() - time_tick;
+    time_diff_ms = sl_sleeptimer_tick_to_ms(time_diff_tick);
+
+    mbedtls_printf( "\n  . OK  clock cycles: %ld, time: %" PRIu32 " ms\n\n" );
+
+exit:
+    mbedtls_rsa_free( &rsa );
+}
+
+void app_rsa_verify()
+{
+    int ret = 1;
+    mbedtls_rsa_context rsa;
+    unsigned char hash[32];
+
+    mbedtls_rsa_init( &rsa );
+
+    mbedtls_printf( "\n  . Importing public key" );
+
+
+    if( ( ret = mbedtls_rsa_import( &rsa, &N, NULL, NULL, NULL, &E ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_import returned %d\n\n",
+                        ret );
+        goto exit;
+    }
+
+    rsa.MBEDTLS_PRIVATE(len) = ( mbedtls_mpi_bitlen( &rsa.MBEDTLS_PRIVATE(N) ) + 7 ) >> 3;
+
+    /*
+     * Compute the SHA-256 hash of the input file and
+     * verify the signature
+     */
+    mbedtls_printf( "\n  . Verifying the RSA/SHA-256 signature" );
+
+
+    if( ( ret = mbedtls_md(
+                    mbedtls_md_info_from_type( MBEDTLS_MD_SHA256 ),
+                    din, sizeof(din), hash ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! Could hash input\n\n");
+        goto exit;
+    }
+
+    time_tick = sl_sleeptimer_get_tick_count();
+
+    if( ( ret = mbedtls_rsa_pkcs1_verify( &rsa, MBEDTLS_MD_SHA256,
+                                          32, hash, sign ) ) != 0 )
+    {
+        mbedtls_printf( " failed\n  ! mbedtls_rsa_pkcs1_verify returned -0x%0x\n\n", (unsigned int) -ret );
+        goto exit;
+    }
+
+
+    time_diff_tick = sl_sleeptimer_get_tick_count() - time_tick;
+    time_diff_ms = sl_sleeptimer_tick_to_ms(time_diff_tick);
+
+    mbedtls_printf( "\n  . OK (the signature is valid)  clock cycles: %ld, time: %" PRIu32 " ms\n\n" );
+
+exit:
+
+    mbedtls_rsa_free( &rsa );
+}
+
+
+
 
 void app_rsa_encrypt()
 {
@@ -220,28 +288,6 @@ void app_rsa_encrypt()
         goto exit;
     }
 
-#ifdef FILESYSTEM
-    mbedtls_printf( "\n  . Reading public key from rsa_pub.txt" );
-
-
-    if( ( f = fopen( "rsa_pub.txt", "rb" ) ) == NULL )
-    {
-        mbedtls_printf( " failed\n  ! Could not open rsa_pub.txt\n" \
-                "  ! Please run rsa_genkey first\n\n" );
-        goto exit;
-    }
-
-    if( ( ret = mbedtls_mpi_read_file( &N, 16, f ) ) != 0 ||
-        ( ret = mbedtls_mpi_read_file( &E, 16, f ) ) != 0 )
-    {
-        mbedtls_printf( " failed\n  ! mbedtls_mpi_read_file returned %d\n\n",
-                        ret );
-        fclose( f );
-        goto exit;
-    }
-    fclose( f );
-
-#endif
 
     if( ( ret = mbedtls_rsa_import( &rsa, &N, NULL, NULL, NULL, &E ) ) != 0 )
     {
@@ -277,24 +323,6 @@ void app_rsa_encrypt()
     time_diff_tick = sl_sleeptimer_get_tick_count() - time_tick;
     time_diff_ms = sl_sleeptimer_tick_to_ms(time_diff_tick);
 
-#ifdef FILESYSTEM
-    /*
-     * Write the signature into result-enc.txt
-     */
-    if( ( f = fopen( "result-enc.txt", "wb+" ) ) == NULL )
-    {
-        mbedtls_printf( " failed\n  ! Could not create %s\n\n", "result-enc.txt" );
-        goto exit;
-    }
-
-    for( i = 0; i < rsa.MBEDTLS_PRIVATE(len); i++ )
-        mbedtls_fprintf( f, "%02X%s", encrypted_buf[i],
-                 ( i + 1 ) % 16 == 0 ? "\r\n" : " " );
-
-    fclose( f );
-
-    mbedtls_printf( "\n  . Done (created \"%s\")\n\n", "result-enc.txt" );
-#endif
     mbedtls_printf( " ok clock cycles: %ld, time: %" PRIu32 " ms.\n\n", time_diff_tick, time_diff_ms );
 
 exit:
@@ -302,21 +330,8 @@ exit:
     mbedtls_entropy_free( &entropy );
     mbedtls_rsa_free( &rsa );
 }
-#endif /* MBEDTLS_BIGNUM_C && MBEDTLS_RSA_C && MBEDTLS_ENTROPY_C &&
-          MBEDTLS_FS_IO && MBEDTLS_CTR_DRBG_C */
 
 
-#if !defined(MBEDTLS_BIGNUM_C) || !defined(MBEDTLS_RSA_C) ||  \
-    !defined(MBEDTLS_ENTROPY_C) || \
-    !defined(MBEDTLS_CTR_DRBG_C)
-int app_rsa_decrypt( void )
-{
-    mbedtls_printf("MBEDTLS_BIGNUM_C and/or MBEDTLS_RSA_C and/or "
-           "MBEDTLS_FS_IO and/or MBEDTLS_ENTROPY_C and/or "
-           "MBEDTLS_CTR_DRBG_C not defined.\n");
-    mbedtls_exit( 0 );
-}
-#else
 
 
 void app_rsa_decrypt(  )
@@ -350,33 +365,6 @@ void app_rsa_decrypt(  )
         goto exit;
     }
 
-#ifdef FILESYSTEM
-    mbedtls_printf( "\n  . Reading private key from rsa_priv.txt" );
-
-
-    if( ( f = fopen( "rsa_priv.txt", "rb" ) ) == NULL )
-    {
-        mbedtls_printf( " failed\n  ! Could not open rsa_priv.txt\n" \
-                "  ! Please run rsa_genkey first\n\n" );
-        goto exit;
-    }
-
-    if( ( ret = mbedtls_mpi_read_file( &N , 16, f ) )  != 0 ||
-        ( ret = mbedtls_mpi_read_file( &E , 16, f ) )  != 0 ||
-        ( ret = mbedtls_mpi_read_file( &D , 16, f ) )  != 0 ||
-        ( ret = mbedtls_mpi_read_file( &P , 16, f ) )  != 0 ||
-        ( ret = mbedtls_mpi_read_file( &Q , 16, f ) )  != 0 ||
-        ( ret = mbedtls_mpi_read_file( &DP , 16, f ) ) != 0 ||
-        ( ret = mbedtls_mpi_read_file( &DQ , 16, f ) ) != 0 ||
-        ( ret = mbedtls_mpi_read_file( &QP , 16, f ) ) != 0 )
-    {
-        mbedtls_printf( " failed\n  ! mbedtls_mpi_read_file returned %d\n\n",
-                        ret );
-        fclose( f );
-        goto exit;
-    }
-    fclose( f );
-#endif
 
     if( ( ret = mbedtls_rsa_import( &rsa, &N, &P, &Q, &D, &E ) ) != 0 )
     {
@@ -391,31 +379,7 @@ void app_rsa_decrypt(  )
                         ret );
         goto exit;
     }
-#ifdef FILESYSTEM
-    /*
-     * Extract the RSA encrypted value from the text file
-     */
-    if( ( f = fopen( "result-enc.txt", "rb" ) ) == NULL )
-    {
-        mbedtls_printf( "\n  ! Could not open %s\n\n", "result-enc.txt" );
-        goto exit;
-    }
 
-    i = 0;
-
-    while( fscanf( f, "%02X", (unsigned int*) &c ) > 0 &&
-           i < (int) sizeof( encrypted_buf ) )
-        encrypted_buf[i++] = (unsigned char) c;
-
-    fclose( f );
-
-
-    if( i != rsa.MBEDTLS_PRIVATE(len) )
-    {
-        mbedtls_printf( "\n  ! Invalid RSA signature format\n\n" );
-        goto exit;
-    }
-#endif
     /*
      * Decrypt the encrypted RSA data and print the result.
      */
@@ -450,4 +414,6 @@ exit:
     mbedtls_mpi_free( &DQ ); mbedtls_mpi_free( &QP );
 
 }
-#endif /* MBEDTLS_BIGNUM_C && MBEDTLS_RSA_C && MBEDTLS_FS_IO */
+
+
+
